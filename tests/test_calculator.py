290ae7628335f77a5aac80fd535333d804c9ad1c
BASMA FERNI <fernibasma@gmail.com>
2026-03-29 14:21:09 +0200

    Fix: correction des assertions dans les tests

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,16 +1,15 @@
 import sys
 import os
-
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from app.calculator import addition, multiplication
 
 def test_addition():
-    assert addition(2, 3)
-    assert addition(-6, 1)
-    assert addition(0, 0)
+    assert addition(2, 3) == 5   # ✅ On compare le résultat
+    assert addition(-1, 1) == 0  # ✅
+    assert addition(0, 0) == 0   # ✅ 0 == 0, ça passe !
 
 def test_multiplication():
-    assert multiplication(2, 6)
-    assert multiplication(4, 3)
-    assert multiplication(0, 2)
+    assert multiplication(2, 3) == 6   # ✅
+    assert multiplication(4, 3) == 12  # ✅
+    assert multiplication(0, 2) == 0   #
